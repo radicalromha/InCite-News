e5744c54674a0676dcc998cb8f3e5aa4e28083fe
Eskinder Romha Fitsim <romha223@gmail.com>
2024-05-15 09:37:36 -0500

    removed webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import react from "react";
-import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
