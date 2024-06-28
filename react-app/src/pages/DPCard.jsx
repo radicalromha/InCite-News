c41d1bd214f74f5cb3a30f082ad51616119b9eab
Eskinder Romha Fitsim <romha223@gmail.com>
2024-06-27 21:22:42 -0400

    added undp card

--- react-app/src/pages/DPCard.jsx
+++ react-app/src/pages/DPCard.jsx
@@ -14,39 +14,47 @@ import {
   Link,
 } from "@chakra-ui/react";
 import HumanRights from "../assets/humanrights.jpg";
+import DevelopmentProgram from "../assets/devlopmentprogram.jpg";
 
 function DevCard() {
   return (
     <Card maxW="ex">
       <CardBody>
-        <Image src={HumanRights} alt="Human Rights" />
+        <Image
+          src={DevelopmentProgram}
+          alt="United Nations Development Programme"
+        />
         <Stack mt="6" spacing="3">
           <Heading size="md">
-            High Commissioner for Human Rights (OHCHR)
+            The United Nations Development Programme (UNDP)
           </Heading>
           <Text>
-            Human rights are rights we have simply because we exist as human
-            beings - they are not granted by any state. These universal rights
-            are inherent to us all, regardless of nationality, sex, national or
-            ethnic origin, color, religion, language, or any other status. They
-            range from the most fundamental - the right to life - to those that
-            make life worth living, such as the rights to food, education, work,
-            health, and liberty. The Universal Declaration of Human Rights
-            (UDHR), adopted by the UN General Assembly in 1948, was the first
-            legal document to set out the fundamental human rights to be
-            universally protected. The UDHR, which turns 75 on 10 December 2023,
-            continues to be the foundation of all international human rights
-            law. Its 30 articles provide the principles and building blocks of
-            current and future human rights conventions, treaties and other
-            legal instruments.
+            The UNDP tackles a wide range of global challenges by focusing on
+            interconnected strategies. It addresses poverty and inequality by
+            investing in people’s capabilities to move above the poverty line
+            and sustain progress. To enhance governance, UNDP helps countries
+            manage risk and adapt to emerging complexities with future-proofing
+            strategies. It supports building resilience against shocks such as
+            conflict, climate change, and epidemics. UNDP prioritizes
+            environmental sustainability by integrating nature into national
+            planning and management. It works to increase energy access and
+            promote a just transition to sustainable energy systems. The
+            organization also champions gender equality by transforming
+            economic, political, and social structures that perpetuate
+            discrimination. In the digital realm, UNDP supports the creation of
+            inclusive, ethical, and sustainable digital societies. It drives
+            strategic innovation to make systems adaptive and resilient and
+            partners with various sectors to align financial flows with
+            sustainable development goals, mobilizing finance at scale for
+            impactful development.
           </Text>
         </Stack>
       </CardBody>
       <CardFooter>
         <Flex justify="center" width="100%">
-          <Link href="https://www.ohchr.org/en/ohchr_homepage" isExternal>
+          <Link href="https://www.undp.org/" isExternal>
             <Button variant="solid" colorScheme="blue">
-              Visit ohchr
+              Visit undp
             </Button>
           </Link>
         </Flex>
