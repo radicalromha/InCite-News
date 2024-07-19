64b3b79b23531efd42927e7a97a2228c69f0b2c3
Eskinder Romha Fitsim <romha223@gmail.com>
2024-07-18 23:10:26 -0400

    firebase deployment worked?

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import { ChakraProvider } from "@chakra-ui/react";
-
 import { NextUIProvider } from "@nextui-org/react";
 
 createRoot(document.getElementById("root")).render(
