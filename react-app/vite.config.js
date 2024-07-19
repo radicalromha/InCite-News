0f96b0e0b726eb32f6535e08559062772fd3cf3e
Eskinder Romha Fitsim <romha223@gmail.com>
2024-07-18 21:15:57 -0400

    change build dir in vite config

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
+  build: {
+    outDir: "dist",
   },
 });
